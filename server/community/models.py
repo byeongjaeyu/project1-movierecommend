9d517d89f6d69b8bd8b3b2684705c156913f4740
byeongjae <ybj121725@gmail.com>
2021-11-17 12:59:59 +0900

    1117 1259

---
+++ server/community/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+
+class Comment(models.Model):
+    content = models.TextField
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)

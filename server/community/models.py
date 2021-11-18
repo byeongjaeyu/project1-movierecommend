24caa7952cab8c1b3f152f50c80aa25caec254a2
byeongjae <ybj121725@gmail.com>
2021-11-18 16:05:22 +0900

    1118 1605 bh

--- server/community/models.py
+++ server/community/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class Review(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    rank = models.IntegerField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)

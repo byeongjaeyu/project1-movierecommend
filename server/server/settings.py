f66b35ec54484dfdb8bd5a6141eb41e82cab6aee
youngmin <kgjdudals@naver.com>
2021-11-18 09:18:20 +0900

    cors 셋팅추가

--- server/server/settings.py
+++ server/server/settings.py
@@ -34,7 +34,10 @@ INSTALLED_APPS = [
     'accounts',
     'community',
     'movies',
+
     'rest_framework',
+    'corsheaders',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,3 +147,11 @@ REST_FRAMEWORK = {
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA' : datetime.timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'https://example.com',
+    'https://sub.example.com',
+    'http://localhost:8080',
+    'http://127.0.0.1:9000'
+]
+CORS_ALLOW_ALL_ORIGINS = True

f33fa1ef53845fa01f19a02a2e83a3159127baa3
byeongjae <ybj121725@gmail.com>
2021-11-18 11:52:51 +0900

    1118 1152 bj

--- client/src/views/movies/Index.vue
+++ client/src/views/movies/Index.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <h1>최신영화20개</h1>
+    <div>
+      <div v-for="movie in movies" :key="movie.movie_id">
+        {{ movie.title }}
+        {{ movie.poster_path }}
+        <img :src="posterUrl" alt="">
+        <hr>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,18 +17,24 @@ import axios from 'axios'
 
 export default {
   name: 'Index',
+  data: function () {
+    return {
+      movies : [],
+    }
+  },
   created: function () {
     axios({
       method: 'get',
       url: 'http://127.0.0.1:8000/movies/'
     })
       .then(res => {
-        console.log(res)
+        this.movies = res.data
+        console.log(this.movies)
       })
       .catch(err => {
         console.log(err)
       })
-  }
+  },
 }
 </script>
 

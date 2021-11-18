d56c11e7dd19acae22d4ca996f3600fdbe6697eb
youngmin <kgjdudals@naver.com>
2021-11-18 11:24:51 +0900

    파일정리

--- server/movies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.movie_index),
-    path('recommend1/', views.random_recommend),
-    path('search/<str:word>/', views.movie_search),
-    path('recommend2/<int:movie_id>/', views.choice_recommend),
-]

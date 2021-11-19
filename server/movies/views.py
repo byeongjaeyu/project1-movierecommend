8dc490966fb95d57938a0739a3ae6604583782be
youngmin <kgjdudals@naver.com>
2021-11-19 11:09:14 +0900

    community 백앤드 구현

--- server/movies/views.py
+++ server/movies/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 import requests
 import random
-import urllib.parse
+
 # Create your views here.
 
 key = "733c7d5145ecf236ad387093e2d52047"
@@ -98,29 +98,30 @@ def movie_search(request, word):
         return Response(serializer.data)
 
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def choice_recommend(request, movie_id):
-    if request.method == 'GET':
-        Url = f"https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key={key}&language=ko-kr&page=1"
-        temp = []
-        responses = requests.get(Url).json()
-        for page in range(1,10):
-            Url = f"https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key={key}&language=ko-kr&page={page}"
-            responses = requests.get(Url).json()
-            for response in responses["results"]:
-                temp_dict = {}
-                temp_dict["title"] = response["title"]
-                temp_dict["release_date"] = response["release_date"]
-                temp_dict["popularity"] = response["popularity"]
-                temp_dict["vote_count"] = response["vote_count"]
-                temp_dict["vote_average"] = response["vote_average"]
-                temp_dict["overview"] = response["overview"]
-                temp_dict["poster_path"] = poster_url + response["poster_path"]
-                temp_dict["genres"] = response["genre_ids"]
-                temp.append(temp_dict)
-        temp.sort(key = lambda x: -x["vote_average"])
-        temp = temp[:20]
-        serializer = MovieSerializer(temp, many=True)
-        return Response(serializer.data)
-
+# @api_view(['GET'])
+# @permission_classes([AllowAny])
+# def genre_recommend(request):
+#     if request.user.is_authenticated:
+#         user = request.user
+#         reviews = user.review_set.all()
+#         like_genre = set()
+#         if reviews.count():
+#             for review in reviews:
+#                 movie = Movie.objects.filter(title=review.movie_title)
+#                 if movie.exists():
+#                     genres = movie[0].genres.all()
+#                     for genre in genres:
+#                         like_genre.add(genre.id)
+#             movies = Movie.objects.none()
+#             for genre in like_genre:
+#                 movie = Movie.objects.filter(genres__in=[genre])
+#                 movies = movies.union(movie, all=False)
+#             movie_list = movies.order_by('vote_average')[:10]
+#         else:
+#             movies = Movie.objects.all()
+#             movie_list = movies.order_by('vote_average')[:10]
+#         print(movie_list)
+#     context = {
+#         'movie_list' : movie_list,
+#     }
+#     return Response(context)
